03d24c3c85c58dff1e81f6c42f50b5483be04813
josueramirez1 <josue67ramirez@gmail.com>
2024-05-01 17:26:12 -0700

    left click functionality partionally completed

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const board = document.querySelector(".board");
+let subtext = document.querySelector(".subtext");
 displayBoard();
 let tiles = [...document.querySelectorAll("[data-status]")];
 let newTiles = shuffle(tiles);
-newBoard(newTiles);
+let mines = newTiles.filter((tile, index) => {
+  if (index <= 10) {
+    return tile;
+  }
+});
 
 // FUNCTIONS
 
 function displayBoard() {
   // hidden class
-  for (let i = 1; i <= 90; i++) {
+  for (let i = 1; i <= 100; i++) {
     let div = document.createElement("div");
     div.setAttribute("data-status", "hidden");
-    board.append(div);
-  }
-  // mine class
-  for (let i = 1; i <= 10; i++) {
-    let div = document.createElement("div");
-    div.setAttribute("data-status", "mine");
     board.append(div);
   }
 }
@@ -43,15 +42,35 @@ function shuffle(array) {
 }
 
 // Left click functionality
+document.addEventListener("click", (e) => {
+  if (!e.target.matches("[data-status]")) return;
+  // if user clicks on non-mine...
+  console.dir(e.target);
+  newTiles.forEach((tile) => {
+    e.target.dataset.status = "number";
+  });
+
+  // if user clicks on mine...
+  mines.forEach((mine) => {
+    if (e.target === mine) {
+      mines.forEach((m) => {
+        m.dataset.status = "mine";
+      });
+      subtext.textContent = "You Lose ☹️ ";
+    }
+  });
+});
 
 // Right click functionality
-
+let count = 10;
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
   if (e.target.matches('[data-status="hidden"]')) {
-    e.target.setAttribute("data-status", "marked");
+    e.target.dataset.status = "marked";
+    subtext.textContent = `Mines Left: ${--count}`;
   } else if (e.target.matches('[data-status="marked"')) {
-    e.target.setAttribute("data-status", "hidden");
+    e.target.dataset.status = "hidden";
+    subtext.textContent = `Mines Left: ${++count}`;
   }
   return false;
 });
