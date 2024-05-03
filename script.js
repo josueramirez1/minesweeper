b29599cc5f9ccccdd541c08b9bd88d1777211ee6
josueramirez1 <josue67ramirez@gmail.com>
2024-05-03 15:44:28 -0700

    changing code

--- script.js
+++ script.js
@@ -1,194 +1,174 @@
 const body = document.querySelector("body");
 const board = document.querySelector(".board");
 let subtext = document.querySelector(".subtext");
-loadBoard();
-let tiles = [...document.querySelectorAll("[data-status]")];
-shuffle(tiles);
-let mines = tiles.filter((tile, index) => {
-  if (index < 10) {
-    return tile;
-  }
-});
-let safeTiles = tiles.filter((tile, index) => {
-  if (index >= 10) {
-    return tile;
-  }
-});
+const width = 10;
+const bombAmount = 10;
+let squares = [];
+
+// shuffle(tiles);
 
 // FUNCTIONS
-
+let rightClick = 10;
 function loadBoard() {
-  // hidden class
-  for (let i = 1; i <= 100; i++) {
-    let div = document.createElement("div");
-    div.dataset.status = "hidden";
-    div.id = i;
-    board.append(div);
+  const bombArray = Array(width).fill("bomb");
+  const emptyArray = Array(width * width - bombAmount).fill("valid");
+  const gameArray = emptyArray.concat(bombArray);
+  const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < width * width; i++) {
+    let square = document.createElement("div");
+    square.dataset.status = "hidden";
+    square.id = i;
+    square.classList.add(shuffledArray[i]);
+    squares.push(square);
+    board.append(square);
+
+    square.addEventListener("click", (e) => {
+      click(square);
+    });
+
+    // Right click functionality
+
+    square.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      if (e.target.matches('[data-status="hidden"]')) {
+        e.target.dataset.status = "marked";
+        subtext.textContent = `Mines Left: ${--rightClick}`;
+      } else if (e.target.matches('[data-status="marked"')) {
+        e.target.dataset.status = "hidden";
+        subtext.textContent = `Mines Left: ${++rightClick}`;
+      }
+      return false;
+    });
+  }
+
+  for (let i = 0; i < squares.length; i++) {
+    let total = 0;
+    const leftSide = i % width === 0;
+    const rightSide = i % width === width - 1;
+
+    if (squares[i].classList.contains("valid")) {
+      if (i > 0 && !leftSide && squares[i - 1].classList.contains("bomb"))
+        total++;
+      if (
+        i > 9 &&
+        !rightSide &&
+        squares[i + 1 - width].classList.contains("bomb")
+      )
+        total++;
+      if (i > 10 && squares[i - width].classList.contains("bomb")) total++;
+      if (
+        i > 11 &&
+        !leftSide &&
+        squares[i - width - 1].classList.contains("bomb")
+      )
+        total++;
+      if (i < 99 && !rightSide && squares[i + 1].classList.contains("bomb"))
+        total++;
+      if (
+        i < 90 &&
+        !leftSide &&
+        squares[i - 1 + width].classList.contains("bomb")
+      )
+        total++;
+      if (
+        i < 88 &&
+        !rightSide &&
+        squares[i + 1 + width].classList.contains("bomb")
+      )
+        total++;
+      if (i < 89 && squares[i + width].classList.contains("bomb")) total++;
+      squares[i].setAttribute("data", total);
+    }
   }
 }
 
-// Fisher-Yates
-function shuffle(array) {
-  let currentIndex = array.length;
-  let temporaryValue, randomIndex;
+loadBoard();
 
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+function click(square) {
+  console.log(square);
+  if (
+    square.matches('[data-status="number') ||
+    square.matches('[data-status="marked"') ||
+    square.classList.contains("checked")
+  ) {
+    return;
   }
 
-  return array;
-}
-
-function topNeighbor(e, array) {
-  let topTile = e.target.id - 10;
-  let tile = array.find((t) => {
-    let numT = parseInt(t.id);
-    if (numT === topTile) {
-      return t;
+  if (square.classList.contains("bomb")) {
+    gameOver();
+  } else {
+    let total = square.getAttribute("data");
+    if (total != 0) {
+      square.classList.add("checked");
+      if (total === 1) square.classList.add("one");
+      if (total === 2) square.classList.add("two");
+      if (total === 3) square.classList.add("three");
+      if (total === 4) square.classList.add("four");
+      square.textContent = total;
     }
-  });
-  return tile;
-}
-
-function topRightNeighbor(e, array) {
-  let topRightTile = e.target.id - 9;
-  let tile = array.find((t) => {
-    let numT = parseInt(t.id);
-    if (numT === topRightTile) {
-      return t;
-    }
-  });
-  return tile;
-}
-function topLeftNeighbor(e, array) {
-  let topLeftTile = e.target.id - 11;
-  let tile = array.find((t) => {
-    let numT = parseInt(t.id);
-    if (numT === topLeftTile) {
-      return t;
-    }
-  });
-  return tile;
-}
-function bottomNeighbor(e, array) {
-  let bottomTile = parseInt(e.target.id) + 10;
-  let tile = array.find((t) => {
-    let numT = parseInt(t.id);
-    if (numT === bottomTile) {
-      return t;
-    }
-  });
-  return tile;
-}
-function bottomRightNeighbor(e, array) {
-  let bottomRightTile = parseInt(e.target.id) + 11;
-  let tile = array.find((t) => {
-    let numT = parseInt(t.id);
-    if (numT === bottomRightTile) {
-      return t;
-    }
-  });
-  return tile;
-}
-function bottomLeftNeighbor(e, array) {
-  let bottomLeftTile = parseInt(e.target.id) + 9;
-  let tile = array.find((t) => {
-    let numT = parseInt(t.id);
-    if (numT === bottomLeftTile) {
-      return t;
-    }
-  });
-  return tile;
-}
-
-// Left click functionality
-board.addEventListener("click", (e) => {
-  let num = 0;
-  let top = topNeighbor(e, tiles);
-  let topR = topRightNeighbor(e, tiles);
-  let topL = topLeftNeighbor(e, tiles);
-  let bottom = bottomNeighbor(e, tiles);
-  let bottomR = bottomRightNeighbor(e, tiles);
-  let bottomL = bottomLeftNeighbor(e, tiles);
-  let safeTop = topNeighbor(e, safeTiles);
-  let safeTopR = topRightNeighbor(e, safeTiles);
-  let safeTopL = topLeftNeighbor(e, safeTiles);
-  let safeBottom = bottomNeighbor(e, safeTiles);
-  let safeBottomR = bottomRightNeighbor(e, safeTiles);
-  let safeBottomL = bottomLeftNeighbor(e, safeTiles);
-
-  if (!e.target.matches("[data-status]")) return;
-
-  tiles.forEach(() => (e.target.dataset.status = "number"));
-
-  mines.forEach((mine) => {
-    if (
-      e.target.nextElementSibling === mine ||
-      e.target.previousElementSibling === mine ||
-      top === mine ||
-      topR === mine ||
-      topL === mine ||
-      bottom === mine ||
-      bottomR === mine ||
-      bottomL === mine
-    ) {
-      num++;
-      e.target.textContent = num;
-    }
-
-    if (e.target === mine) {
-      e.target.dataset.status = "mine";
-      e.target.textContent = "";
-      mines.forEach((m) => {
-        m.dataset.status = "mine";
-      });
-      subtext.textContent = "You Lose ☠️ ";
-    }
-  });
-
-  safeTiles.forEach((safe) => {
-    if (e.target.nextElementSibling === safe) {
-      e.target.nextElementSibling.dataset.status = "number";
-    }
-    if (e.target.previousElementSibling === safe) {
-      e.target.previousElementSibling.dataset.status = "number";
-    }
-    if (safeTop === safe) {
-      safeTop.dataset.status = "number";
-    }
-    if (safeTopR === safe) {
-      safeTopR.dataset.status = "number";
-    }
-    if (safeTopL === safe) {
-      safeTopL.dataset.status = "number";
-    }
-    if (safeBottom === safe) {
-      safeBottom.dataset.status = "number";
-    }
-    if (safeBottomL === safe) {
-      safeBottomL.dataset.status = "number";
-    }
-    if (safeBottomR === safe) {
-      safeBottomR.dataset.status = "number";
-    }
-  });
-});
-
-// Right click functionality
-let count = 10;
-document.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-  if (e.target.matches('[data-status="hidden"]')) {
-    e.target.dataset.status = "marked";
-    subtext.textContent = `Mines Left: ${--count}`;
-  } else if (e.target.matches('[data-status="marked"')) {
-    e.target.dataset.status = "hidden";
-    subtext.textContent = `Mines Left: ${++count}`;
+    // checkSquare(square);
   }
-  return false;
-});
+  square.dataset.status = "number";
+  square.classList.add("checked");
+}
+
+function gameOver() {
+  subtext.textContent = "BOOM! Game Over";
+
+  squares.forEach((s) => {
+    if (s.classList.contains("bomb")) {
+      s.dataset.status = "mine";
+      s.textContent = "💣";
+      s.classList.remove("bomb");
+      s.classList.add("checked");
+    }
+  });
+}
+
+function checkSquare(square) {
+  const currentId = square.id;
+
+  setTimeout(function () {
+    if (currentId > 0) {
+      const newId = parseInt(currentId) - 1;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId > 9) {
+      const newId = parseInt(currentId) + 1 - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId > 10) {
+      const newId = parseInt(currentId) - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId > 11) {
+      const newId = parseInt(currentId) - 1 - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 98) {
+      const newId = parseInt(currentId) - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 90) {
+      const newId = parseInt(currentId) - 1 + width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 88) {
+      const newId = parseInt(currentId) + 1 + width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    if (currentId < 89) {
+      const newId = parseInt(currentId) + width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+  }, 10);
+}
